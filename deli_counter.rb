1ecf45cdae8268b6cea18803b0502487662ced8e
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-19 16:07:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(number)
   if number.length == 0
     puts "The line is currently empty."
   else
-    number.each.with_index(1) do |name, index|
+     number.each.with_index(1) do |name, index|
       line_number.push("#{index}. #{name}")
     end
     puts "The line is currently:#{line_number.join("")}"
