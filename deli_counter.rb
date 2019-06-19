985df08eb4ae2638c2949d4a2d733384572316b2
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-19 16:17:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,4 @@ def take_a_number(katz_deli, name)
   katz_deli<<(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+take_a_number(["jim","sarah","bill"], "stephanie")
