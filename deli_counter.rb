f8084407c5210d80126b3cd21b08b91b26f645ef
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-19 16:28:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,5 +20,9 @@ end
 
 def now_serving(line)
   if line == 0
-    puts 
+    puts puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}."
+    line.shift
+  end
 end
