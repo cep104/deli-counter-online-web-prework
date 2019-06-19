1e37d74d116c23df0071f5e01f08a108d3aad0af
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-19 16:29:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,11 +18,11 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(line)
-  if line.empty?
-    puts puts "There is nobody waiting to be served!"
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}."
-    line.shift
+    puts "Currently serving #{array[0]}."
+    array.shift
   end
 end
