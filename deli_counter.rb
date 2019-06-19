44c84c66173690cf4211d90e7570040c4dc3be45
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-19 15:47:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,9 @@
-def line(katz_deli)
+def line(number)
   katz_deli_line = []
   index = 1
+  if number >1 
+    katz_deli_line.none? do 
+      
 katz_deli.each do |index|
      katz_deli_line.push("#{index +=1}")
 
