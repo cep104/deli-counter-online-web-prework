05472c3549da96ef68b94316bf5b1c59ca8e50fc
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-19 15:55:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,3 @@
-def line(katz_deli_line, number)
-  katz_deli_line = []
-  index = 1
-  if number < 1 
- puts "The line is currently empty."
-else
-  number.each_with_index do |index|
-     katz_deli_line.push("#{index +=1}")
-   
-end
-katz_deli_line
-end
+def line(number)
+  if number.length == 0 
+    
